c03a9fcd08230be2d89d9da7db3d8f7e63821e10
Dach-h <bunny.hop.md@gmail.com>
2011-12-13 09:47:48 +0900

    refactor

--- lib/lokka/tweet_feed.rb
+++ lib/lokka/tweet_feed.rb
@@ -105,95 +105,107 @@ module Lokka
       end
     end
 
-    def self.consumer
-      OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, :site => "http://twitter.com")
-    end
-
-    def self.configure
-      Twitter.configure do |config|
-        config.consumer_key       = CONSUMER_KEY
-        config.consumer_secret    = CONSUMER_SECRET
-        config.oauth_token        = Option.tweet_feed_token
-        config.oauth_token_secret = Option.tweet_feed_secret
+    class << self
+      def consumer
+        OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, :site => "http://twitter.com")
       end
-    end
 
-    def self.when_register(post, url)
-      return nil unless Option.tweet_feed_token && Option.tweet_feed_secret
-      post.reload
-      if post.draft
-        Lokka::TweetFeed.check_draft(post.id)
-      else
-        Lokka::TweetFeed.tweet(post, url)
+      def configure
+        Twitter.configure do |config|
+          config.consumer_key       = CONSUMER_KEY
+          config.consumer_secret    = CONSUMER_SECRET
+          config.oauth_token        = Option.tweet_feed_token
+          config.oauth_token_secret = Option.tweet_feed_secret
+        end
       end
-    end
 
-    def self.when_update(post, url)
-      return nil unless Lokka::TweetFeed.uncontribution?(post.id)
-      unless post.draft
-        Lokka::TweetFeed.tweet(post,url)
+      def when_register(post, url)
+        validation Lokka::TweetFeed.auth? do
+          post.reload
+          if post.draft
+            Lokka::TweetFeed.check_draft(post.id)
+          else
+            Lokka::TweetFeed.tweet(post, url)
+          end
+        end
       end
-    end
 
-    def self.tweet(post, long_url)
-      site, url = Site.first, long_url + "/#{post.id}"
-      short_url = Lokka::TweetFeed.short_url(url)
-      Lokka::TweetFeed.configure
-      Twitter.update("#{Option.tweet_feed_post_message}: #{post.title} - #{site.title} #{short_url}")
-      Lokka::TweetFeed.reject(post.id)
-    end
+      def when_update(post, url)
+        validation Lokka::TweetFeed.unregistered?(post.id) do
+          unless post.draft
+            Lokka::TweetFeed.tweet(post,url)
+          end
+        end
+      end
 
-    def self.short_url(long_url)
-      case Option.tweet_feed_short_url
-      when  "bitly"
-        Lokka::TweetFeed.bitly_short_url(long_url)
-      when "tiny"
-        Lokka::TweetFeed.tiny_short_url(long_url)
-      else
+      def tweet(post, long_url)
+        site, url = Site.first, long_url + "/#{post.id}"
+        short_url = Lokka::TweetFeed.short_url(url)
+        Lokka::TweetFeed.configure
+        Twitter.update("#{Option.tweet_feed_post_message}: #{post.title} - #{site.title} #{short_url}")
+        Lokka::TweetFeed.reject(post.id)
+      end
+
+      def short_url(long_url)
+        case Option.tweet_feed_short_url
+        when  "bitly"
+          Lokka::TweetFeed.bitly_short_url(long_url)
+        when "tiny"
+          Lokka::TweetFeed.tiny_short_url(long_url)
+        else
+          long_url
+        end
+      end
+
+      def bitly_short_url(long_url)
+        user_id, api_key = Option.tweet_feed_bitly_user_id, Option.tweet_feed_bitly_api_key
+        query   = "version=2.0.1&longUrl=#{long_url}&login=#{user_id}&apiKey=#{api_key}"
+        results = JSON.parse(Net::HTTP.get("api.bit.ly", "/shorten?#{query}"))
+        results["results"][long_url]["shortUrl"]
+      rescue
         long_url
       end
-    end
 
-    def self.bitly_short_url(long_url)
-      user_id, api_key = Option.tweet_feed_bitly_user_id, Option.tweet_feed_bitly_api_key
-      query   = "version=2.0.1&longUrl=#{long_url}&login=#{user_id}&apiKey=#{api_key}"
-      results = JSON.parse(Net::HTTP.get("api.bit.ly", "/shorten?#{query}"))
-      results["results"][long_url]["shortUrl"]
-    rescue
-      long_url
-    end
-
-    def self.tiny_short_url(long_url)
-      query = "url=#{URI.encode(long_url)}"
-      Net::HTTP.get("tinyurl.com", "/api-create.php?#{query}")
-    rescue
-      long_url
-    end
-
-    def self.check_draft(id)
-      if Option.tweet_feed_uncontribution
-        Option.tweet_feed_uncontribution = Lokka::TweetFeed.uncontribution << id
-      else
-        Option.tweet_feed_uncontribution = [id]
+      def tiny_short_url(long_url)
+        query = "url=#{URI.encode(long_url)}"
+        Net::HTTP.get("tinyurl.com", "/api-create.php?#{query}")
+      rescue
+        long_url
       end
-    end
 
-    def self.uncontribution
-      result = Option.tweet_feed_uncontribution
-      eval(result)
-    end
-
-    def self.uncontribution?(id)
-      if Option.tweet_feed_uncontribution
-        Lokka::TweetFeed.uncontribution.include?(id)
-      else
-        false
+      def check_draft(id)
+        if Option.tweet_feed_unregistered
+          Option.tweet_feed_unregistered = Lokka::TweetFeed.unregistered << id
+        else
+          Option.tweet_feed_unregistered = [id]
+        end
       end
-    end
 
-    def self.reject(id)
-      if Option.tweet_feed_token && Option.tweet_feed_secret && Lokka::TweetFeed.uncontribution?(id)
-        Option.tweet_feed_uncontribution = Lokka::TweetFeed.uncontribution.reject{|i| i == id}
+      def validation(obj = nil, &block)
+        return nil unless obj
+        block.call
+      end
+
+      def auth?
+        Option.tweet_feed_token && Option.tweet_feed_secret
+      end
+
+      def unregistered
+        eval Option.tweet_feed_unregistered
+      end
+
+      def unregistered?(id)
+        if Option.tweet_feed_unregistered
+          Lokka::TweetFeed.unregistered.include?(id)
+        else
+          false
+        end
+      end
+
+      def reject(id)
+        if Lokka::TweetFeed.auth? && Lokka::TweetFeed.unregistered?(id)
+          Option.tweet_feed_unregistered = Lokka::TweetFeed.unregistered.reject{|i| i == id}
+        end
       end
     end
   end
